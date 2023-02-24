a7e77f947e42bd94396aa5f5e5393c2f778320aa
Putri Felicia Tiara Maharani <114546568+putrifelicia16@users.noreply.github.com>
2023-02-24 08:15:02 +0700

    Add files via upload

---
+++ array_buah.php
@@ -0,0 +1,32 @@
+<?php
+//cara penulisan array
+$ar_buah = array('pisang', 'mangga', 'jeruk');
+
+// array indexed
+$buah = ['alpukat', 'jeruk','mangga','pisang'];
+// cara manggil array indexed
+echo $buah[1];
+
+// cetak jumlah buah 
+echo '<br/>Jumlah buah' .count($buah);
+
+// cetak seluruh buah
+echo "<ol>";
+foreach($buah as $fruit){
+    echo "<li>$fruit</li>";
+}
+echo "</ol>";
+
+//================
+// tambahkan buah baru ke dalam array
+$buah[] = "Durian";
+
+// cetak seluruh buah
+echo "<ol>";
+
+    foreach ($buah as $buas){
+        echo "<li> $buas </li>";
+    }
+
+    echo "</ol>";
+?>
